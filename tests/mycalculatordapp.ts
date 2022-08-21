f70a99fd91426ea7ae586d9798057da2a43ff6d4
willowsenator <willowsenator@gmail.com>
2022-08-21 21:57:14 +0200

    Execute prettier to format files

--- tests/mycalculatordapp.ts
+++ tests/mycalculatordapp.ts
@@ -1,6 +1,6 @@
 import * as anchor from "@project-serum/anchor";
 import * as assert from "assert";
-const {SystemProgram} = anchor.web3
+const { SystemProgram } = anchor.web3;
 
 describe("mycalculatordapp", () => {
   // Configure the client to use the local cluster.
@@ -9,16 +9,18 @@ describe("mycalculatordapp", () => {
   const calculator = anchor.web3.Keypair.generate();
   const program = anchor.workspace.Mycalculatordapp;
 
-  it('Create a calculator', async()=>{
-    await program.rpc.create('Welcome to Solana', {
-        accounts: {
-            calculator: calculator.publicKey,
-            user: provider.wallet.publicKey,
-            systemProgram: SystemProgram.programId
-        },
-        signers: [calculator]
+  it("Create a calculator", async () => {
+    await program.rpc.create("Welcome to Solana", {
+      accounts: {
+        calculator: calculator.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [calculator],
     });
-      const account = await program.account.calculator.fetch(calculator.publicKey);
-      assert.ok(account.greeting === 'Welcome to Solana');
-  })
+    const account = await program.account.calculator.fetch(
+      calculator.publicKey
+    );
+    assert.ok(account.greeting === "Welcome to Solana");
+  });
 });
